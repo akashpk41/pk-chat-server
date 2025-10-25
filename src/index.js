9fad743fa76984cb0f69f9a1ae11ac78bb3db204
akashpk41 <akashpk84100@gmail.com>
2025-10-25 18:02:52 +0600

    updated cors

--- src/index.js
+++ src/index.js
@@ -11,7 +11,12 @@ import { connectDB } from "./lib/db.js";
 // configuration
 dotenv.config({ quiet: true });
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -7,7 +7,7 @@ export const protectedRoute = async (req, res, next) => {
     if (!token) {
       return res
         .status(401)
-        .json({ message: "Unauthorized - No Token Provided!" });
+        .json({ message: "Unauthorized - No Token Provided!!!" });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
